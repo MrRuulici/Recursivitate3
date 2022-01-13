d787f8aa188285670da4d5799045495523e2be3b
Raul Ulici <raululici@yahoo.com>
2022-01-13 16:25:16 +0200

    final

---
+++ Recursivitate3/Fibonacci.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursivitate3
+{
+    class Fibonacci
+    {
+        public static int Fib(int a, int b, int n, int p)
+        {
+            if (n == p)
+                return a;
+            else
+                return Fib(b, a + b, n + 1, p);
+        }
+    }
+}
